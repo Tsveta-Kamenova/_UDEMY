f8d666a092f28e5aa6a17ad396ec49e349fa5549
Tsveta-Kamenova <walkerintherain@gmail.com>
2022-04-05 22:08:14 +0300

    overloading

--- OOP/method overloading.py
+++ OOP/method overloading.py
@@ -5,4 +5,22 @@ class student:
     def disp(self):
         print("Welcome")
 
+
 obj = student()
+obj.disp()
+
+
+# obj.disp(23, "Alex")
+
+
+class overload:
+
+    def fun(self, a, b=None):
+        print(a)
+
+    def fun(self, a, b=None):
+        print(a,b)
+
+obj=overload()
+obj.fun(89)
+obj.fun(23, 45)
