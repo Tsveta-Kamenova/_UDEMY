3b5278e9436752ec4d0a36a75b133a9e4ca53962
Tsveta-Kamenova <walkerintherain@gmail.com>
2022-04-06 12:52:43 +0300

    abstraction classes

--- OOP/method overriding.py
+++ OOP/method overriding.py
@@ -1,15 +1,17 @@
 class myclass:
 
     def fun(self, n1, n2):
-        return n1+n2
+        return n1 + n2
+
 
 class child(myclass):
 
     def fun(self, n1, n2):
-        return n1*n2
+        return n1 * n2
+
 
 obj = child()
 print(obj.fun(2, 3))
 
 objp = myclass()
-print(objp.fun(2,3))
+print(objp.fun(2, 3))
